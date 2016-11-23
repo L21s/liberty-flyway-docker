52afee0063c4e680d906bf513e821fbd56684836
tobilarscheid <tobilarscheid@gmail.com>
2016-11-23 18:32:58 +0100

    fix replacement

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-./flyway/flyway -url=jDATABASEc:mysql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME -user=$DATABASE_USER -password=$DATABASE_PASSWORD -locations=filesystem:./DATABASE/migration migrate && /opt/ibm/docker/docker-server run defaultServer
+./flyway/flyway -url=jdbc:mysql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME -user=$DATABASE_USER -password=$DATABASE_PASSWORD -locations=filesystem:./DATABASE/migration migrate && /opt/ibm/docker/docker-server run defaultServer
